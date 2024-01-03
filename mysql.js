2b7a5ae5719b7abb1d7f83472fcb4e6572200727
hyemin12 <66300732+hyemin12@users.noreply.github.com>
2024-01-03 17:34:18 +0900

    fix: 도서 전체 조회 오류 수정(condition이 undefined일 경우에 나타나는 오류)

--- mysql.js
+++ mysql.js
@@ -4,9 +4,9 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  database: "bookstore",
+  host: process.env.HOST,
+  user: process.env.USER,
+  database: process.env.DATABASE,
   password: process.env.DATABASE_PASSWORD,
 });
 
