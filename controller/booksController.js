b40cb759aadf5046822fc6fab9f4dd1ba4848d47
고혜민 <66300732+hyemin12@users.noreply.github.com>
2024-01-04 20:38:09 +0900

    refactor: 주석 삭제

--- controller/booksController.js
+++ controller/booksController.js
@@ -2,25 +2,6 @@ const { StatusCodes } = require("http-status-codes");
 const getSqlQueryResult = require("../utils/getSqlQueryResult");
 const handleServerError = require("../utils/handleServerError");
 
-// MySQL은 LIMIT 및 OFFSET 절에서 바인딩 매개변수를 지원하지 않습니다. 그래서 이러한 오류가 발생하는 것입니다. 따라서, 바인딩 매개변수 대신 직접 값을 쿼리에 삽입하는 방식으로 해결해야 합니다.
-
-// 하지만 직접 값을 쿼리에 삽입하는 경우 SQL 인젝션 공격에 취약해질 수 있으므로, 반드시 값을 검증하고 안전하게 변환해야 합니다.
-
-// 아래는 안전하게 LIMIT과 OFFSET 값을 쿼리에 삽입하는 방법입니다:
-// const limit = 6;
-// const offset = limit * (page - 1);
-
-// // 숫자로 변환하고, NaN이거나 음수인 경우 기본값으로 설정
-// const safeLimit = Number.isNaN(limit) || limit < 0 ? 10 : limit;
-// const safeOffset = Number.isNaN(offset) || offset < 0 ? 0 : offset;
-
-// const sql = `
-//     SELECT * FROM books
-//     WHERE title LIKE CONCAT("%", ?, "%")
-//     LIMIT ${safeLimit} OFFSET ${safeOffset}`;
-
-// const { rows, conn } = await getSqlQueryResult(sql, ['모던']);
-
 const getBooks = async (req, res) => {
   const { categoryId, new: fetchNewBooks, page } = req.query;
 
