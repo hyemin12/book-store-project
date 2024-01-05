d86ddd7a08040fdd26dbac97221cb66078616fd7
hyemin12 <66300732+hyemin12@users.noreply.github.com>
2024-01-05 14:35:06 +0900

    refactor: prettier, eslint 설치 후 코드 포맷팅

--- controller/categoryController.js
+++ controller/categoryController.js
@@ -1,18 +1,17 @@
-const { StatusCodes } = require("http-status-codes");
-const conn = require("../mysql");
+const { StatusCodes } = require('http-status-codes');
 
-const getSqlQueryResult = require("../utils/getSqlQueryResult");
-const handleServerError = require("../utils/handleServerError");
+const getSqlQueryResult = require('../utils/getSqlQueryResult');
+const handleServerError = require('../utils/handleServerError');
 
 const getCategory = async (req, res) => {
-  const sql = "SELECT * FROM category";
-  try {
-    const { rows, conn } = await getSqlQueryResult(sql);
-    res.status(StatusCodes.OK).send({ lists: rows });
-    conn.release();
-  } catch (err) {
-    handleServerError(res, err);
-  }
+	const sql = 'SELECT * FROM category';
+	try {
+		const { rows, conn } = await getSqlQueryResult(sql);
+		res.status(StatusCodes.OK).send({ lists: rows });
+		conn.release();
+	} catch (err) {
+		handleServerError(res, err);
+	}
 };
 
 module.exports = { getCategory };
--- controller/likesController.js
+++ controller/likesController.js
@@ -1,45 +1,48 @@
-const { StatusCodes } = require("http-status-codes");
+const { StatusCodes } = require('http-status-codes');
 
-const getSqlQueryResult = require("../utils/getSqlQueryResult");
-const handleServerError = require("../utils/handleServerError");
+const getSqlQueryResult = require('../utils/getSqlQueryResult');
+const handleServerError = require('../utils/handleServerError');
 
 const postLike = async (req, res, next) => {
-  const { bookId } = req.params;
-  const { user_id } = req.body;
-  const sql = "INSERT INTO likes (user_id, book_id) VALUES (?, ?)";
-  const values = [user_id, bookId];
-  try {
-    const { rows, conn } = await getSqlQueryResult(sql, values);
-    if (rows.affectedRows > 0) {
-      res.status(StatusCodes.CREATED).send({ message: "좋아요 추가 성공" });
-    } else {
-      res.status(StatusCodes.BAD_REQUEST).send({ message: "좋아요 추가 실패" });
-    }
-    conn.release();
-  } catch (err) {
-    handleServerError(res, err);
-  }
+	const { bookId } = req.params;
+	const { user_id } = req.body;
+
+	const sql = 'INSERT INTO likes (user_id, book_id) VALUES (?, ?)';
+	const values = [user_id, bookId];
+
+	try {
+		const { rows, conn } = await getSqlQueryResult(sql, values);
+		if (rows.affectedRows > 0) {
+			res.status(StatusCodes.CREATED).send({ message: '좋아요 추가 성공' });
+		} else {
+			res.status(StatusCodes.BAD_REQUEST).send({ message: '좋아요 추가 실패' });
+		}
+		conn.release();
+	} catch (err) {
+		handleServerError(res, err);
+	}
 };
 
 const deleteLike = async (req, res, next) => {
-  const { bookId } = req.params;
-  const { user_id } = req.body;
+	const { bookId } = req.params;
+	const { user_id } = req.body;
 
-  const sql = `DELETE FROM likes
+	const sql = `DELETE FROM likes
         WHERE user_id = ? AND book_id = ?`;
-  const values = [user_id, bookId];
-  try {
-    const { rows, conn } = await getSqlQueryResult(sql, values);
+	const values = [user_id, bookId];
 
-    if (rows.affectedRows > 0) {
-      res.status(StatusCodes.CREATED).send({ message: "좋아요 삭제 성공" });
-    } else {
-      res.status(StatusCodes.BAD_REQUEST).send({ message: "좋아요 삭제 실패" });
-    }
-    conn.release();
-  } catch (err) {
-    handleServerError(res, err);
-  }
+	try {
+		const { rows, conn } = await getSqlQueryResult(sql, values);
+
+		if (rows.affectedRows > 0) {
+			res.status(StatusCodes.CREATED).send({ message: '좋아요 삭제 성공' });
+		} else {
+			res.status(StatusCodes.BAD_REQUEST).send({ message: '좋아요 삭제 실패' });
+		}
+		conn.release();
+	} catch (err) {
+		handleServerError(res, err);
+	}
 };
 
 module.exports = { postLike, deleteLike };
