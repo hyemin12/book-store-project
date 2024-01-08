6a4f4e940f680d24e43ae92a0a849bf97cf0688c
hyemin12 <66300732+hyemin12@users.noreply.github.com>
2024-01-08 12:32:13 +0900

    refactor: 코드 포맷팅

--- routes/users.js
+++ routes/users.js
@@ -1,26 +1,26 @@
-const express = require("express");
+const express = require('express');
 const {
-  joinUser,
-  loginUser,
-  requestResetPassword,
-  resetPassword,
-} = require("../controller/usersController");
+	joinUser,
+	loginUser,
+	requestResetPassword,
+	resetPassword
+} = require('../controller/usersController');
 const {
-  validatesLoginAndReset,
-  validatesJoin,
-  validatesEmail,
-} = require("../validators/users");
+	validatesLoginAndReset,
+	validatesJoin,
+	validatesEmail
+} = require('../validators/users');
 
 const router = express.Router();
 router.use(express.json());
 
-router.post("/join", validatesJoin, joinUser);
+router.post('/join', validatesJoin, joinUser);
 
-router.post("/login", validatesLoginAndReset, loginUser);
+router.post('/login', validatesLoginAndReset, loginUser);
 
 router
-  .route("/reset")
-  .post(validatesEmail, requestResetPassword)
-  .put(validatesLoginAndReset, resetPassword);
+	.route('/reset')
+	.post(validatesEmail, requestResetPassword)
+	.put(validatesLoginAndReset, resetPassword);
 
 module.exports = router;
--- utils/handleServerError.js
+++ utils/handleServerError.js
@@ -1,8 +1,10 @@
-const { StatusCodes } = require("http-status-codes");
+const { StatusCodes } = require('http-status-codes');
 
 const handleServerError = (res, error) => {
-  console.error(error);
-  res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({ message: "Internal Server Error" });
+	console.error(error);
+	res
+		.status(StatusCodes.INTERNAL_SERVER_ERROR)
+		.send({ message: 'Internal Server Error' });
 };
 
 module.exports = handleServerError;
