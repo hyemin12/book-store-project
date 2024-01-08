8b8f3dac14ca0d1c46e788cf02362a673b416784
hyemin12 <66300732+hyemin12@users.noreply.github.com>
2024-01-08 12:54:45 +0900

    feat: 카트 유효성 검사 내용 추가 (카트 상품, 도서 아이디, 유저 아이디가 존재하는지)

--- validators/carts.js
+++ validators/carts.js
@@ -5,13 +5,37 @@ const validateUserId = body('user_id')
 	.trim()
 	.notEmpty()
 	.isNumeric()
-	.withMessage('user_id는 number 타입이어야 함');
+	.withMessage('user_id는 number 타입이어야 함')
+	.custom(async (user_id) => {
+		const sql = 'SELECT * FROM users WHERE id = ?';
+		const { conn, rows } = await getSqlQueryResult(sql, [user_id]);
+
+		if (!rows.length) {
+			throw new Error('일치하는 회원이 없음');
+		}
+
+		conn.release();
+
+		return true;
+	});
 
 const validateBookId = body('book_id')
 	.trim()
 	.notEmpty()
 	.isNumeric()
-	.withMessage('book_id는 number 타입이어야 함');
+	.withMessage('book_id는 number 타입이어야 함')
+	.custom(async (book_id) => {
+		const sql = 'SELECT * FROM books WHERE id = ?';
+		const { conn, rows } = await getSqlQueryResult(sql, [book_id]);
+
+		if (!rows.length) {
+			throw new Error('일치하는 book_id가 없음');
+		}
+
+		conn.release();
+
+		return true;
+	});
 
 const validateQuantity = body('quantity')
 	.trim()
@@ -23,7 +47,19 @@ const validateId = param('id')
 	.trim()
 	.notEmpty()
 	.isNumeric()
-	.withMessage('id는 number 타입이어야 함');
+	.withMessage('id는 number 타입이어야 함')
+	.custom(async (id) => {
+		const sql = 'SELECT * FROM cartItems WHERE id = ?';
+		const { conn, rows } = await getSqlQueryResult(sql, [id]);
+
+		if (!rows.length) {
+			throw new Error('일치하는 상품이 없음');
+		}
+
+		conn.release();
+
+		return true;
+	});
 
 const validateAddToCart = [
 	validateUserId,
