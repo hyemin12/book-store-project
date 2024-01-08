6dd9449357453a355b1189ebe754220404fe0e13
hyemin12 <66300732+hyemin12@users.noreply.github.com>
2024-01-08 12:31:52 +0900

    feat: cart 유효성 검사 로직 추가

--- validators/carts.js
+++ validators/carts.js
@@ -1,8 +1,47 @@
-const { param, query } = require('express-validator');
+const { body, param } = require('express-validator');
 const validateAndProceed = require('./validateAndProceed');
 
-const validateBookId = query('categoryId')
+const validateUserId = body('user_id')
 	.trim()
 	.notEmpty()
 	.isNumeric()
-	.withMessage('categoryId는 number 타입이어야 함');
+	.withMessage('user_id는 number 타입이어야 함');
+
+const validateBookId = body('book_id')
+	.trim()
+	.notEmpty()
+	.isNumeric()
+	.withMessage('book_id는 number 타입이어야 함');
+
+const validateQuantity = body('quantity')
+	.trim()
+	.notEmpty()
+	.isNumeric()
+	.withMessage('quantity는 number 타입이어야 함');
+
+const validateId = param('id')
+	.trim()
+	.notEmpty()
+	.isNumeric()
+	.withMessage('id는 number 타입이어야 함');
+
+const validateAddToCart = [
+	validateUserId,
+	validateBookId,
+	validateQuantity,
+	validateAndProceed
+];
+const validateGetCartsItems = [validateUserId.optional(), validateAndProceed];
+const validateDeleteCartsItem = [validateId, validateAndProceed];
+const validateUpdateCartItemCount = [
+	validateId,
+	validateQuantity,
+	validateAndProceed
+];
+
+module.exports = {
+	validateAddToCart,
+	validateGetCartsItems,
+	validateDeleteCartsItem,
+	validateUpdateCartItemCount
+};
