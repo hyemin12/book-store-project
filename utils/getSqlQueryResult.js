a2174c5e0ef48bb4757014e546cc18d0b4af9127
hyemin12 <66300732+hyemin12@users.noreply.github.com>
2024-01-09 12:16:27 +0900

    feat: 결제하기 API

--- utils/getSqlQueryResult.js
+++ utils/getSqlQueryResult.js
@@ -1,7 +1,10 @@
 const mysql = require('../mysql');
 
-const getSqlQueryResult = async (sql, values) => {
-	const conn = await mysql.getConnection();
+const getSqlQueryResult = async (sql, values, connection) => {
+	let conn;
+	if (!connection) {
+		conn = await mysql.getConnection();
+	}
 	const [rows] = await conn.execute(sql, values);
 	return { conn, rows };
 };
